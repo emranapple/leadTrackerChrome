4addc0dad5f4683b8ae986af4352933c07b3c029
Emran <khanemranpau@gmail.com>
2024-01-31 18:37:02 +0600

    save from tab feature is implemented

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
+let myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEL = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-let myLeads = []
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteAllBtn = document.getElementById("delete-all")
+const saveTabBtn = document.getElementById("save-tab-btn")
 
-inputBtn.addEventListener('click', function() {
-    myLeads.push(inputEL.value)
-    renderLeads()
-    clearInputField()
-    
+// saveTabBtn 
+saveTabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderLeads(myLeads)
+    })
 })
 
-function renderLeads() {
+// deletebtn 
+deleteAllBtn.addEventListener("dblclick", function (e) {
+    e.preventDefault()
+    localStorage.clear()
+    console.log("doubled click")
+    myLeads = []
+    renderLeads(myLeads)
+})
+
+// save myleads to localStorage 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
+
+
+}
+
+// inputBtn 
+inputBtn.addEventListener('click', function () {
+    myLeads.push(inputEL.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads)
+    clearInputField()
+})
+
+// render function 
+function renderLeads(leads) {
     let listItem = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItem += `
             <li>
-                <a target="_blank"href="${myLeads[i]}">${myLeads[i]}</a>
+                <a target="_blank"href="${leads[i]}">${leads[i]}</a>
             </li>
             `
     }
     ulEl.innerHTML = listItem
 }
 
-
+// clear inputfield function 
 function clearInputField() {
     inputEL.value = ""
 }
@@ -30,5 +61,11 @@ function clearInputField() {
 
 
 
-// now needs to store the value after refresh the page how?
+
+
+// Maybe in near future I will make it to firebase 
+// also make menifest file to run 
+
+
+
 
